3e4fb24502a83a9e59faaa06801aa6150b2e5667
isile <tkdgus6986@naver.com>
2024-06-20 18:54:57 +0900

    2024-06-20
    학습기록

---
+++ PrjMySql/src/main/java/com/green/menu/service/MenuService.java
@@ -0,0 +1,21 @@
+package com.green.menu.service;
+
+import java.util.List;
+
+import com.green.menu.vo.MenuVo;
+
+public interface MenuService {
+
+	List<MenuVo> getMenuList();
+
+	void insertMenu(MenuVo vo);
+
+	MenuVo getMenu(MenuVo vo);
+
+	void updateMenu(MenuVo vo);
+
+	void deleteMenu(MenuVo vo);
+
+	void insertMenu2(MenuVo vo);
+
+}
